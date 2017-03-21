864c81a6062ee4814ff3995960595a7091680fc9
rjoseph01 <robert.joseph@nyu.edu>
2017-03-21 07:49:49 -0400

    commit 2
    functionality

--- js/scripts.js
+++ js/scripts.js
@@ -14,47 +14,62 @@
       .on('done', function(layer) {
 
         // get layer from Carto
-        var acc_2015 = layer.getSubLayer(0);
-
-
+        var acc_2015 = layer.getSubLayer(1);
+        acc_2015.setSQL('SELECT cartodb_id, the_geom, the_geom_webmercator, name, ve_total FROM acc_15wmetro');
+        // $('map').ready(function() {
+        //   setSQL('SELECT * FROM acc_15wmetro');
+        // });
 
         // event listeners to change the SQL for the carto layer
         $('#VTB1').on('click', function() {
-           acc_2015.setSQL('SELECT the_geom, the_geom_webmercator, name, ve_total FROM acc_15wmetro WHERE ve_total = 1');
-           alert('this should work');
+          var VTB1 = 'SELECT cartodb_id, the_geom, the_geom_webmercator, name, ve_total FROM acc_15wmetro WHERE ve_total = 1'
+           acc_2015.setSQL(VTB1);   
         });
-        $('#VTB2').on('click', function() {
-           acc_2015.setSQL('SELECT the_geom, the_geom_webmercator, name, ve_total FROM acc_15wmetro WHERE ve_total = 2');
-        });
-         $('#VTB3').on('click', function() {
-           acc_2015.setSQL('SELECT the_geom, the_geom_webmercator, name, ve_total FROM acc_15wmetro WHERE ve_total = 3');
-        });
-         $('#VTB4').on('click', function() {
-           acc_2015.setSQL('SELECT the_geom, the_geom_webmercator, name, ve_total FROM acc_15wmetro WHERE ve_total > 3');
+         $('#FTB3').on('click', function() {
+           acc_2015.setSQL('SELECT cartodb_id, the_geom, the_geom_webmercator, name, ve_total, fatals FROM acc_15wmetro WHERE fatals = 3');
         });
 
-
-        // $('.reset').on('click', function() {
-
-
-        //   acc_2015.setSQL('SELECT the_geom, the_geom_webmercator, agency, agency_nam, complaint_, descriptor FROM table_311_1 WHERE agency = \'TLC\'');
-        // });
+         
+         //Reset Button
+        $('#reset').on('click', function() {
+        acc_2015.setSQL('SELECT cartodb_id, the_geom, the_geom_webmercator, name, ve_total FROM acc_15wmetro');
+        });
 
         // set interactivity
          acc_2015.setInteraction(true);
-         acc_2015.setInteractivity('nameslad');
+         acc_2015.setInteractivity('name');
 
         // Interactivity for this layer
          acc_2015.on('featureClick', function(e, latlng, pos, data, layerNumber) {
-          //$('#SCID').empty();
+          $('#SCID').empty();
           $('#SCID').append('Metro Area: ' + data.name + '<br/>')
           //$('#AD').append('Descriptor: ' + data.descriptor)
         });
 
-         var acc_count = acc_2015.setSQL('SELECT count(*) FROM acc_15wmetro');
-        console.log(acc_count);
-        $('#AD').empty();
-        $('#AD').append(acc_count);
+       
+        $('#VTB2').on('click', function() {
+           acc_2015.setSQL('SELECT cartodb_id, the_geom, the_geom_webmercator, name, ve_total FROM acc_15wmetro WHERE ve_total = 2');
+        });
+         $('#VTB3').on('click', function() {
+           acc_2015.setSQL('SELECT cartodb_id, the_geom, the_geom_webmercator, name, ve_total FROM acc_15wmetro WHERE ve_total = 3');
+        });
+         $('#VTB4').on('click', function() {
+           acc_2015.setSQL('SELECT cartodb_id, the_geom, the_geom_webmercator, name, ve_total FROM acc_15wmetro WHERE ve_total > 3');
+        });
+        $('#FTB1').on('click', function() {
+           acc_2015.setSQL('SELECT cartodb_id, the_geom, the_geom_webmercator, name, ve_total, fatals FROM acc_15wmetro WHERE fatals = 1');   
+        });
+        $('#FTB2').on('click', function() {
+           acc_2015.setSQL('SELECT cartodb_id, the_geom, the_geom_webmercator, name, ve_total, fatals FROM acc_15wmetro WHERE fatals = 2');
+        });
+
+          var sql = new cartodb.SQL({ user: 'raj331' });
+          sql.execute('SELECT count(*) FROM acc_15wmetro')
+          .done(function(data) {
+            console.log(data.rows[0].count);
+            $('#AD').empty();
+            $('#AD').append(data.rows[0].count);
+          });
 
       }).on('error', function() {
         //log the error
@@ -63,21 +78,21 @@
 
     // Use the SQL API to get raw data from carto
 
-    var apiCall = 'https://raj331.carto.com/api/v2/sql?q=SELECT%20*%20FROM%20acc_15wmetro%20WHERE%20cartodb_id=1'
+    // var apiCall = 'https://raj331.carto.com/api/v2/sql?q=SELECT%20*%20FROM%20acc_15wmetro%20WHERE%20cartodb_id=1'
 
-    $.getJSON(apiCall, function(data) {
-      console.log(data);
-    })
+    // $.getJSON(apiCall, function(data) {
+    //   console.log(data);
+    // })
 
-    // geojson API CALL
+    // // geojson API CALL
 
-    var geojsonApiCall = 'https://raj331.carto.com/api/v2/sql?q=SELECT%20*%20FROM%20acc_15wmetro%20WHERE%20cartodb_id=23454&format=geojson';
+    // var geojsonApiCall = 'https://raj331.carto.com/api/v2/sql?q=SELECT%20*%20FROM%20acc_15wmetro%20WHERE%20cartodb_id=23454&format=geojson';
 
-    $.getJSON(geojsonApiCall, function(data) {
-      console.log(data);
+    // $.getJSON(geojsonApiCall, function(data) {
+    //   console.log(data);
 
-      L.geoJson(data).addTo(map);
-    });
+    //   L.geoJson(data).addTo(map);
+    // });
 
 //Sidebar Button Panel Operation
 //Default Hidden Panels
